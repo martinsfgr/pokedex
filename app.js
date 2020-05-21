adb3afe8fc75f8eafbea1a993b7e500626a4aa1e
Fagner <mauriciofagner2010@hotmail.com>
2020-05-21 08:07:49 -0300

    Gerar o card corretamente com informações detalhadas de cada pokemon

--- app.js
+++ app.js
@@ -12,11 +12,17 @@ const fetchPokemons = () => {
   Promise.all(pokemonPromises)
     .then(pokemons => {
       const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-        accumulator += `<li>${pokemon.name}</li>`
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+        accumulator += `
+          <li class="card">
+            <img class="card-image ${types[0]} alt="${pokemon.name} src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')}</p>
+          </li>
+        `
         return accumulator
       }, '')
-
-      console.log(lisPokemons)
     })
 }
 
