b0b01ddf0aea730075d5ba4f7dc868cd0cb3d532
Fagner <mauriciofagner2010@hotmail.com>
2020-05-21 08:13:52 -0300

    Renderizar os pokemons dentro do html

--- app.js
+++ app.js
@@ -15,14 +15,17 @@ const fetchPokemons = () => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
         accumulator += `
-          <li class="card">
-            <img class="card-image ${types[0]} alt="${pokemon.name} src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+          <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
             <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
             <p class="card-subtitle">${types.join(' | ')}</p>
           </li>
         `
         return accumulator
       }, '')
+
+      const ul = document.querySelector('ul.pokedex')
+      ul.innerHTML = lisPokemons
     })
 }
 
