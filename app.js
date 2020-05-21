e96cce3c8e9d4cd78046b0ead2ed227668fd1455
Fagner <mauriciofagner2010@hotmail.com>
2020-05-21 08:27:44 -0300

    Refatorar o código

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const fetchPokemons = () => {
-  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-  const pokemonPromises = []
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-  for (let i = 1; i <= 150; i++) {
-    pokemonPromises.push(
-      fetch(getPokemonUrl(i))
-        .then(response => response.json())
-    )
-  }
+const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+  fetch(getPokemonUrl(index + 1)).then(response => response.json())
+)
 
-  Promise.all(pokemonPromises)
-    .then(pokemons => {
-      const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+const generateHTML = pokemons => {
+  return lisPokemons = pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-        accumulator += `
-          <li class="card ${types[0]}">
-            <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
-            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-            <p class="card-subtitle">${types.join(' | ')}</p>
-          </li>
-        `
-        return accumulator
-      }, '')
-
-      const ul = document.querySelector('ul.pokedex')
-      ul.innerHTML = lisPokemons
-    })
+    accumulator += `
+      <li class="card ${elementTypes[0]}">
+        <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
+        <h2 class="card-title">${id}. ${name}</h2>
+        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+      </li>
+    `
+    return accumulator
+  }, '')
 }
 
-fetchPokemons()
+const insertPokemonsIntoPage = pokemons => {
+  const ul = document.querySelector('ul.pokedex')
+  ul.innerHTML = pokemons
+}
+
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+  .then(generateHTML)
+  .then(insertPokemonsIntoPage)
