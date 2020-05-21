0b1609874b77adb923412a1cbd535d96e3fc2ba4
Fagner <mauriciofagner2010@hotmail.com>
2020-05-21 07:45:14 -0300

    Fazer a requisição dos 150 pokemons

---
+++ app.js
@@ -0,0 +1,18 @@
+const fetchPokemons = () => {
+  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+  const pokemonPromises = []
+
+  for (let i = 1; i <= 150; i++) {
+    pokemonPromises.push(
+      fetch(getPokemonUrl(i))
+        .then(response => response.json())
+    )
+  }
+
+  Promise.all(pokemonPromises)
+    .then(pokemons => {
+      console.log(pokemons)
+    })
+}
+
+fetchPokemons()
